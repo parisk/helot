82f5ecae1698c0634e0fe26d15573aa527f503ca
Paris <paris@sourcelair.com>
2015-12-25 22:10:32 +0200

    Add template manipulation

--- helot.js
+++ helot.js
@@ -1,7 +1,10 @@
 module.exports = {};
 
 var csv = require('csv-parser');
+var fm = require('front-matter');
 var fs = require('fs');
+var md = require('markdown-it')('commonmark');
+var mustache = require('mustache');
 
 function getRecipients(text) {
   var csvParserStream = csv();
@@ -26,5 +29,35 @@ function getRecipientsFromFile(path) {
   return getRecipients(text);
 }
 
+function getTemplate(text) {
+  var parsedTemplate = fm(text);
+  var template = {
+    template: parsedTemplate.body,
+    data: parsedTemplate.attributes
+  };
+
+  return template;
+}
+
+function getTemplateFromFile(path) {
+  var templateText = fs.readFileSync(path);
+
+  return getTemplate(templateText.toString('utf-8'));
+}
+
+function renderTemplate(template, data) {
+  template = mustache.render(template, data);
+
+  var renderedTemplate = {
+    text: template,
+    html: md.render(template)
+  };
+
+  return renderedTemplate;
+}
+
 module.exports.getRecipientsFromFile = getRecipientsFromFile;
 module.exports.getRecipients = getRecipients;
+module.exports.getTemplate = getTemplate;
+module.exports.getTemplateFromFile = getTemplateFromFile;
+module.exports.renderTemplate = renderTemplate;
